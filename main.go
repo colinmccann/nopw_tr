a7e39caeb48d6bc7ac4e587f2457468b0af80e35
Colin <colindmccann@gmail.com>
2021-12-10 12:30:26 -0500

    prebuilt for linux, simplifying again

--- main.go
+++ main.go
@@ -2,27 +2,25 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv4"
 )
 
 func main() {
-	// open up the listening addres for returning ICMP packets. Or is this two way somehow?
+	// open up the listening address for returning ICMP packets. Or is this two way somehow?
 	c, err := icmp.ListenPacket("udp4", "0.0.0.0")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer c.Close()
 
-	wm := icmp.Message{
-		Type: ipv4.ICMPTypeEcho, Code: 0,
-		Body: &icmp.Echo{
-			ID: os.Getpid() & 0xffff, Seq: 1,
-			Data: []byte("HELLO-R-U-THERE"),
-		},
-	}
+	// wm := icmp.Message{
+	// 	Type: ipv4.ICMPTypeEcho, Code: 0,
+	// 	Body: &icmp.Echo{
+	// 		ID: os.Getpid() & 0xffff, Seq: 1,
+	// 		Data: []byte("HELLO-R-U-THERE"),
+	// 	},
+	// }
 
 	// wb, err := wm.Marshal(nil)
 	// if err != nil {
@@ -32,18 +30,18 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	rb := make([]byte, 1500)
-	n, peer, err := c.ReadFrom(rb)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// rb := make([]byte, 1500)
+	// n, peer, err := c.ReadFrom(rb)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	rm, err := icmp.ParseMessage(58, rb[:n])
-	if err != nil {
-		log.Fatal(err)
-	}
+	// rm, err := icmp.ParseMessage(58, rb[:n])
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	log.Printf("Received rm - %+v - from peer - %+v", rm, peer)
+	// log.Printf("Received rm - %+v - from peer - %+v", rm, peer)
 
 	// client := &http.Client{
 	// 	Transport: &http.Transport{
