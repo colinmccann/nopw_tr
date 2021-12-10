e82cd7ba84cdf85ee3094371c3e778a955ffcde8
Colin <colindmccann@gmail.com>
2021-12-10 13:46:32 -0500

    windows version using net instead of icmp package

--- main.go
+++ main.go
@@ -11,11 +11,20 @@ import (
 )
 
 func main() {
+	// TODO - create an init/setup func, put this in
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	// open up the listening address for returning ICMP packets. Or is this two way somehow?
-	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	// c should be renamed socket?
+	// c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	icmpSocket, err := net.ListenPacket("ip4:icmp", "0.0.0.0")
+
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer icmpSocket.Close()
+
+	c := ipv4.NewPacketConn(icmpSocket)
 	defer c.Close()
 
 	wm := icmp.Message{
@@ -31,24 +40,29 @@ func main() {
 		log.Fatal(err)
 
 	}
-	if _, err := c.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP("142.1.217.155"), Zone: "en0"}); err != nil {
+	// if _, err := c.WriteTo(wb, nil, &dst); err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Could not send the ICMP packet: %s\n", err)
+	// 	os.Exit(1)
+	// }
+	if _, err := c.WriteTo(wb, nil, &net.UDPAddr{IP: net.ParseIP("142.1.217.155"), Zone: "en0"}); err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("Got here")
 
-	// rb := make([]byte, 1500)
-	// n, peer, err := c.ReadFrom(rb)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	readBuffer := make([]byte, 1500)
+	// n, peer, err := c.ReadFrom(readBuffer)
+	readBytes, _, hopNode, err := c.ReadFrom(readBuffer)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// rm, err := icmp.ParseMessage(58, rb[:n])
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	readMessage, err := icmp.ParseMessage(58, readBuffer[:readBytes])
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// log.Printf("Received rm - %+v - from peer - %+v", rm, peer)
+	log.Printf("Received readMessage - %+v - from hopNode - %+v", readMessage, hopNode)
 
 	// client := &http.Client{
 	// 	Transport: &http.Transport{
