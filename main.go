f6ba2fc30308423401f0aecd4ea7d8f4e73b1b23
Colin <colindmccann@gmail.com>
2021-12-10 12:26:38 -0500

    prebuilt for linux, should identify the package issue if it exists

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"os"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// open up the listening addres for returning ICMP packets. Or is this two way somehow?
+	c, err := icmp.ListenPacket("udp4", "0.0.0.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1,
+			Data: []byte("HELLO-R-U-THERE"),
+		},
+	}
+
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := c.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP("142.1.217.155"), Zone: "en0"}); err != nil {
+		log.Fatal(err)
+	}
+
+	rb := make([]byte, 1500)
+	n, peer, err := c.ReadFrom(rb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rm, err := icmp.ParseMessage(58, rb[:n])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Received rm - %+v - from peer - %+v", rm, peer)
+
+	// client := &http.Client{
+	// 	Transport: &http.Transport{
+	// 		DialContext: dialer.DialContext,
+	// 	},
+	// }
+
+	// resp, err := client.Get("http://ixmaps.ca")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp.Body.Close()
+
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(string(body))
 }
